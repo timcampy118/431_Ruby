e2f01846df372c85dd4f001e82b56748b1073762
Tri Pham <phamminhtris@tamu.edu>
2020-10-23 17:58:26 -0500

    Add responsive map (auto-resize)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-// import * as d3 from "./d3/d3.js";
-// import * as topojson from "./topojson/topojson.js";
-
 var svgWidth = 1000
 var svgHeight = 700
 
@@ -14,10 +11,11 @@ var chartMargin = {
 var width = svgWidth - chartMargin.left - chartMargin.right;
 var height = svgHeight - chartMargin.top - chartMargin.bottom;
 
-var svg = d3.select(".content")
+var svg = d3.select("#content")
 	.append("svg")
 	.attr("width", width)
-	.attr("height", height);
+	.attr("height", height)
+	.call(responsivefy);
 
 // Map drawing function
 // Data from "https://github.com/topojson/us-atlas"
@@ -83,3 +81,31 @@ d3.json("data/counties-10m.json").then(function (data) {
 	})
 });
 
+// Help with responsive chart
+// source: https://brendansudol.com/writing/responsive-d3
+function responsivefy(svg) {
+    // get container + svg aspect ratio
+    var container = d3.select(svg.node().parentNode),
+        width = parseInt(svg.style("width")),
+        height = parseInt(svg.style("height")),
+        aspect = width / height;
+
+    // add viewBox and preserveAspectRatio properties,
+    // and call resize so that svg resizes on inital page load
+    svg.attr("viewBox", "0 0 " + width + " " + height)
+        .attr("perserveAspectRatio", "xMinYMid")
+        .call(resize);
+
+    // to register multiple listeners for same event type, 
+    // you need to add namespace, i.e., 'click.foo'
+    // necessary if you call invoke this function for multiple svgs
+    // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+    d3.select(window).on("resize." + container.attr("id"), resize);
+
+    // get width of container and resize svg to fit it
+    function resize() {
+        var targetWidth = parseInt(container.style("width"));
+        svg.attr("width", targetWidth);
+        svg.attr("height", Math.round(targetWidth / aspect));
+    }
+}
